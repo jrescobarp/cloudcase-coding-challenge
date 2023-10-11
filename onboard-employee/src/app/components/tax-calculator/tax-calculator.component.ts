8d8cc132991019f5ab2556440e986f3bb2aaebd8
Jose Escobar <escobar7jose@gmail.com>
2023-10-11 13:53:44 -0600

    Added Calculator logic and initial styling

--- onboard-employee/src/app/components/tax-calculator/tax-calculator.component.ts
+++ onboard-employee/src/app/components/tax-calculator/tax-calculator.component.ts
@@ -6,22 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./tax-calculator.component.scss']
 })
 export class TaxCalculatorComponent {
-  salary = 0;
-  retirement_contribution_percentage = 0;
-  annual_tax = 0;
-  annual_retirement_contribution = 0;
-  total = 0;
+  salary: number = 0;
+  retirement_contribution_percentage: number = 0;
+  annual_tax: number = 0;
+  annual_retirement_contribution: number = 0;
+  total: number = 0;
+  displayResult = false;
 
   calculateSalary(){
-    console.log("Salary", this.salary);
+    let tempSalary = this.salary;
+    this.annual_tax = 0;
     // 0-18200 = no tax
     // 18201-37000 = .19 per dollar > 18200
     // 37001-90000 = .325 per dollar > 37000, +3572
     // 90001-180000 = .37 per dollar > 90000, +20797
     // 180001+ = .45 per dollar > 180000, +54097
-    // if(this.salary <= 18200){
-    //   this.annual_tax = 0;
-    // }else if()
+
+    if(tempSalary >= 180001){
+      //calculate per dollar amount > 180000
+      this.annual_tax += (tempSalary - 180000) * .45;
+      // add set amount for being > 180000
+      this.annual_tax += 54097;
+      // set salary to 180000 to calculate additional tax from other taxt brackets
+      tempSalary = 180000;
+      // these comments apply to all following if statements with respect to new taxable values
+    }else if(90001 <= tempSalary && tempSalary < 180001 ){
+      this.annual_tax += (tempSalary - 90000) * .37;
+      this.annual_tax += 20797;
+      tempSalary = 90000;
+    }else if(37001 <= tempSalary && tempSalary < 90001 ){
+      this.annual_tax += (tempSalary - 37000) * .325;
+      this.annual_tax += 3572;
+      tempSalary = 37000;
+    }else if(18201 <= tempSalary && tempSalary < 37001 ){
+      this.annual_tax += (tempSalary - 18200) * .19;
+    }
   }
 
   calculateRetirement(){
@@ -30,8 +49,9 @@ export class TaxCalculatorComponent {
   }
 
   totalCost(){
-    this.total = this.salary + this.annual_tax + this.annual_retirement_contribution;
+    this.total = +this.salary + +this.annual_tax + +this.annual_retirement_contribution;
     console.log("total", this.total);
+    this.displayResult = true;
   }
 
 }
